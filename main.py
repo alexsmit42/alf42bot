8253ea811b50d3f6f2604db8f682ca22c1ddb45f
AlexSmith <avalk87@gmail.com>
2017-08-12 22:57:48 +0300

    print

--- main.py
+++ main.py
@@ -25,17 +25,7 @@ def main():
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
  
-        if today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, u'Доброе утро, {}'.format(last_chat_name))
-            today += 1
- 
-        elif today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, u'Добрый день, {}'.format(last_chat_name))
-            today += 1
- 
-        elif today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, u'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
+        greet_bot.send_message(last_chat_id, u'Доброе утро, {} (В ответ на: {})'.format(last_chat_name, last_chat_text))
  
         new_offset = last_update_id + 1
  
